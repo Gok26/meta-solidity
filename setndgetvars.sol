68ad772893732384eca1195603ef48c6c82e8e00
Gok26 <68271091+Gok26@users.noreply.github.com>
2023-03-29 15:08:43 +0530

    Create setndgetvars.sol

---
+++ setndgetvars.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Vars{
+
+    int a;
+    uint b;
+    string s;
+    bool bl;
+    
+    function setVal1(int _val1) public {
+         a = _val1;
+    }
+    function setVal2(uint _val2) public{
+        b=_val2;
+    }
+    function setString(string memory _val3) public{
+        s=_val3;
+    }
+    function setBool(bool _b) public{
+        bl=_b;
+    }
+
+
+    function getUnit() public view returns(uint){
+        return a;
+    }
+    function getInt() public view returns(int){
+        return b;
+    }
+    function getString() public view returns(bool){
+        return s;
+    }
+    function getBoolean() public view returns(string memory){
+        return bl;
+    }
+}
